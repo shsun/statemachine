dc56c2c469d154d844040f4b788d2bbb453c4969
shsun <89090125@qq.com>
2019-05-06 19:41:18 +0800

    fmt

--- example.py
+++ example.py
@@ -9,86 +9,88 @@ from statemachine import FSM
 # 
 # 
 def on_state_entering(parameters, state):
-	print str(parameters), 'is_running:', fsm.is_running, ' has_executed:', state.has_executed()
-	if state == ready:
-		time.sleep( 0.5 )
-		# TODO
-		next = A0
-		return (next, {"prev_state":state, "message":"current: "+state.name+", next: "+next.name})
-	elif state == A0:
-		time.sleep( 0.5 )
-		# TODO
-		next = B
-		return (next, {"prev_state":state, "message":"current: "+state.name+", next: "+next.name}) 
-	elif state == B:
-		time.sleep( 0.5 )
-		# TODO
-		next = C
-		return (next, {"prev_state":state, "message":"current: "+state.name+", next: "+next.name})
-	elif state == C:
-		time.sleep( 0.5 )
-		# TODO
-		if random.randint(0, 1) :
-			next = C
-		else :
-			next = A1		
-		return (next, {"prev_state":state, "message":"current: "+state.name+", next: "+next.name})
-	elif state == A1:
-		time.sleep( 0.5 )
-		# TODO
-		if random.randint(0, 1) :
-			next = A0
-		else :
-			next = D
-		return (next, {"prev_state":state, "message":"current: "+state.name+", next: "+next.name})
-	elif state == D:
-		time.sleep( 0.5 )
-		# TODO
-		print 'done'
+    print str(parameters), 'is_running:', fsm.is_running, ' has_executed:', state.has_executed()
+    if state == ready:
+        time.sleep(0.5)
+        # TODO
+        next = A0
+        return (next, {"prev_state": state, "message": "current: " + state.name + ", next: " + next.name})
+    elif state == A0:
+        time.sleep(0.5)
+        # TODO
+        next = B
+        return (next, {"prev_state": state, "message": "current: " + state.name + ", next: " + next.name})
+    elif state == B:
+        time.sleep(0.5)
+        # TODO
+        next = C
+        return (next, {"prev_state": state, "message": "current: " + state.name + ", next: " + next.name})
+    elif state == C:
+        time.sleep(0.5)
+        # TODO
+        if random.randint(0, 1):
+            next = C
+        else:
+            next = A1
+        return (next, {"prev_state": state, "message": "current: " + state.name + ", next: " + next.name})
+    elif state == A1:
+        time.sleep(0.5)
+        # TODO
+        if random.randint(0, 1):
+            next = A0
+        else:
+            next = D
+        return (next, {"prev_state": state, "message": "current: " + state.name + ", next: " + next.name})
+    elif state == D:
+        time.sleep(0.5)
+        # TODO
+        print 'done'
 
 
-def on_state_execute_timesout( state ) :
-	print 'on_state_execute_timesout', state.name
-
-def on_fsm_ended( state ) :
-	print 'on_fsm_ended', state.name
+def on_state_execute_timesout(state):
+    print 'on_state_execute_timesout', state.name
 
 
+def on_fsm_ended(state):
+    print 'on_fsm_ended', state.name
 
-def main( ):
-	global fsm
 
-	global ready
-	global A0
-	global B
-	global C
-	global A1
-	global D
+def main():
+    global fsm
 
-	ready = State("ready", on_state_entering, 1, False)
-	A0 = State("A0", on_state_entering, 2, False)
-	B = State("B", on_state_entering, 2, False)
-	C = State("C", on_state_entering, 2, False)
-	A1 = State("A1", on_state_entering, 1, False)
-	D = State("D", on_state_entering, 1, True)
+    global ready
+    global A0
+    global B
+    global C
+    global A1
+    global D
 
-	# 
-	fsm = FSM(on_state_execute_timesout, on_fsm_ended)
-	# add states
-	fsm.add_state(ready)
-	fsm.add_state(A0)
-	fsm.add_state(B)
-	fsm.add_state(C)
-	fsm.add_state(A1)
-	fsm.add_state(D)
-	#
-	fsm.set_start(ready)
-	print 'is_running:', fsm.is_running
+    ready = State("ready", on_state_entering, 1, False)
+    A0 = State("A0", on_state_entering, 2, False)
+    B = State("B", on_state_entering, 2, False)
+    C = State("C", on_state_entering, 2, False)
+    A1 = State("A1", on_state_entering, 1, False)
+    D = State("D", on_state_entering, 1, True)
 
-	#
-	fsm.run( {"prev_state":None, "message":"let's go"} )
-	#fsm.run("x")
-	#fsm.run("x")
+    #
+    fsm = FSM(on_state_execute_timesout, on_fsm_ended)
+    # add states
+    fsm.add_state(ready)
+    fsm.add_state(A0)
+    fsm.add_state(B)
+    fsm.add_state(C)
+    fsm.add_state(A1)
+    fsm.add_state(D)
+    #
+    fsm.set_start(ready)
+    print 'is_running:', fsm.is_running
 
-if __name__== "__main__":
-	main( )
+    #
+    fsm.run({"prev_state": None, "message": "let's go"})
+
+
+# fsm.run("x")
+# fsm.run("x")
+
+if __name__ == "__main__":
+    main()
--- statemachine.py
+++ statemachine.py
@@ -1,6 +1,6 @@
 # 111
 
-#222
+# 222
 
 # **************************************************************************************
 # 			                      ______
@@ -25,70 +25,72 @@ __version__ = '1.1'
 __email__ = '89090125@qq.com'
 
 
-class State :
-	#
-	# name
-	# entering_callback : callback function which will be fired when entering.
-	#	
-	def __init__(self, name, entering_callback, max_execute_times=1, is_end_state=False):
-		self.name = name
-		self.entering_callback = entering_callback
-		self.max_execute_times = (1 if max_execute_times < 1 else max_execute_times ) 
-		self.executed_times = 0
-		self.is_end_state = is_end_state
+class State:
+    '''
+    entering_callback : callback function which will be fired when entering.
+    '''
 
-	def has_executed( self ) :
-		return self.executed_times > 0
+    def __init__(self, name, entering_callback, max_execute_times=1, is_end_state=False):
+        self.name = name
+        self.entering_callback = entering_callback
+        self.max_execute_times = (1 if max_execute_times < 1 else max_execute_times)
+        self.executed_times = 0
+        self.is_end_state = is_end_state
 
-class FSM :
-	def __init__(self, on_state_execute_timesout=None, on_fsm_ended=None):
-		# Hasb used to hold all states
-		self.states = {}
-		# Array used to hold all states which can be end state.
-		self.end_states = []
-		self.start_state = None
-		#
-		self.on_state_execute_timesout = on_state_execute_timesout
-		self.on_fsm_ended = on_fsm_ended
-		self.is_running = False
+    def has_executed(self):
+        return self.executed_times > 0
 
-	# add state
-	def add_state(self, state):
-		self.states[state.name] = state
-		if state.is_end_state:
-			self.end_states.append(state.name)
-	
-	# set the start state.
-	def set_start(self, state):
-		self.start_state = state
-	# 
-	# 
-	# parameter the parameter what you want to pass to target-state.
-	def run(self, parameter):
-		#print "run ", str(self.start_state.name)
-		if self.start_state.name in self.states:
-			entering_callback = self.start_state.entering_callback
-		else:
-			raise "InitError", "please call set_start() before call run()"
-		if not self.end_states:
-			raise  "InitError", "the FSM need at least 1 end_state"
-		old_state = self.start_state
-		while 1 :
-			self.is_running = True
-			(new_state, parameter) = old_state.entering_callback(parameter, old_state)
-			old_state.executed_times += 1
-			if old_state.executed_times > old_state.max_execute_times : 
-				#print old_state.name, " reached max execute times ", old_state.max_execute_times 
-				if self.on_state_execute_timesout <> None: 
-					self.on_state_execute_timesout( old_state )
-					self.is_running = False
-				break;
-			if new_state.name in self.end_states:
-				#print "reached ", new_state.name, "which is an end state"
-				if self.on_fsm_ended <> None:
-					self.on_fsm_ended( new_state )
-					self.is_running = False
-				break 
-			else:
-				entering_callback = new_state.entering_callback
-			old_state = new_state
+
+class FSM:
+    def __init__(self, on_state_execute_timesout=None, on_fsm_ended=None):
+        # Hasb used to hold all states
+        self.states = {}
+        # Array used to hold all states which can be end state.
+        self.end_states = []
+        self.start_state = None
+        #
+        self.on_state_execute_timesout = on_state_execute_timesout
+        self.on_fsm_ended = on_fsm_ended
+        self.is_running = False
+
+    # add state
+    def add_state(self, state):
+        self.states[state.name] = state
+        if state.is_end_state:
+            self.end_states.append(state.name)
+
+    # set the start state.
+    def set_start(self, state):
+        self.start_state = state
+
+    #
+    #
+    # parameter the parameter what you want to pass to target-state.
+    def run(self, parameter):
+        # print "run ", str(self.start_state.name)
+        if self.start_state.name in self.states:
+            entering_callback = self.start_state.entering_callback
+        else:
+            raise "InitError", "please call set_start() before call run()"
+        if not self.end_states:
+            raise "InitError", "the FSM need at least 1 end_state"
+        old_state = self.start_state
+        while 1:
+            self.is_running = True
+            (new_state, parameter) = old_state.entering_callback(parameter, old_state)
+            old_state.executed_times += 1
+            if old_state.executed_times > old_state.max_execute_times:
+                # print old_state.name, " reached max execute times ", old_state.max_execute_times
+                if self.on_state_execute_timesout <> None:
+                    self.on_state_execute_timesout(old_state)
+                    self.is_running = False
+                break;
+            if new_state.name in self.end_states:
+                # print "reached ", new_state.name, "which is an end state"
+                if self.on_fsm_ended <> None:
+                    self.on_fsm_ended(new_state)
+                    self.is_running = False
+                break
+            else:
+                entering_callback = new_state.entering_callback
+            old_state = new_state
